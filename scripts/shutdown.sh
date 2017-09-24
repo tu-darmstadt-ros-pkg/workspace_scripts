040f1bf37b0675317b5162c4fb45e91aed1e8b2e
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2017-09-24 17:25:40 +0200

    added kill command for all screens before shutdown

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -4,6 +4,7 @@ echo -n "ARE YOU SURE TO SHUTDOWN >>> '$(hostname)' <<<? [y/N]"
 read -N 1 REPLY
 
 if [[ "$REPLY" = "y" || "$REPLY" = "Y" ]]; then
+  killall screen
   sudo shutdown now -h
 else
   echo ">>> Shutdown request cancelled"
