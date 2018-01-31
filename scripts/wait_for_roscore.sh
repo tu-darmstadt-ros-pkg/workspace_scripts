acedee9e225ae5592c053c421ae92e1a7e991be7
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2018-01-31 16:16:43 +0100

    added output for wait_for_roscore

--- scripts/wait_for_roscore.sh
+++ scripts/wait_for_roscore.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+source $ROSWSS_BASE_SCRIPTS/helper/helper.sh
+
+echo_note "Waiting for roscore to start up..."
 until rostopic list &>/dev/null ; do sleep 1; done
+echo_info "Roscore found."
