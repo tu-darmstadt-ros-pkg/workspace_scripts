9772505eb77b511f6fc2350dca6713d4a33c35b0
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2018-02-12 14:17:17 +0100

    rosinstall command can now handle multiple inputs

--- scripts/helper/rosinstall.sh
+++ scripts/helper/rosinstall.sh
@@ -4,10 +4,18 @@ rosinstall()
 {
     local rosinstall
     rosinstall=$1 # relative path to $ROSWSS_ROOT/$ROSWSS_INSTALL_DIR/optional expected
+    shift
+
     local LAST_PWD
     LAST_PWD=$PWD
-
     cd $ROSWSS_ROOT/src
-    wstool merge -y $ROSWSS_ROOT/$ROSWSS_INSTALL_DIR/optional/${rosinstall}
+
+    while [[ ! -z "$rosinstall" ]]; do
+      echo "> optional/${rosinstall}"
+      wstool merge -y $ROSWSS_ROOT/$ROSWSS_INSTALL_DIR/optional/${rosinstall}
+      rosinstall=$1
+      shift
+    done
+
     cd $LAST_PWD
 }
