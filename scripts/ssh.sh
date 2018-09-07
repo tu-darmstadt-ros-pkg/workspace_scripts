af48191e2f5962c25b9807af1ebf5de864c1ff9b
Martin Oehler <mail@martinoehler.de>
2018-09-07 16:37:06 +0200

    added support for different users on remote pcs

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -2,19 +2,42 @@
 
 source $ROSWSS_BASE_SCRIPTS/helper/helper.sh
 
-# only execute if ROBOT_USER is set
-if [ -z "$ROBOT_USER" ]; then
-    echo_error "ERROR: In order to use the ssh command, please set ROBOT_USER." 
-    exit 1
-fi
-
 if [ "$#" -eq 0 ]; then
     echo "Usage: ssh <HOST> <Command (optional)>"
     exit 1
 fi
 
 host=$1; shift
-user=$ROBOT_USER
+
+# Convert to arrays
+hosts=($ROBOT_HOSTNAMES)
+users=($ROBOT_USERS)
+
+# Check if multiple users are defined
+if [ -z "$ROBOT_USERS" ]; then
+  # if not, look for single user definition (backwards compatibility)
+  if [ -z "$ROBOT_USER" ]; then
+    echo_error "ERROR: In order to use the ssh command, please set ROBOT_USER or ROBOT_USERS." 
+    exit 1
+  else
+    user=$ROBOT_USER
+  fi
+else
+  # Else look for the array index of the host name
+  for idx in "${!hosts[@]}"; do 
+    if [ "${hosts[$idx]}" = "$host" ]; then
+      # Take corresponding user
+      user="${users[$idx]}"
+      break
+    fi
+  done
+fi
+
+# Check if we found a valid user
+if [ -z "$user" ]; then
+  echo_error "Unknown host '$host'"
+  exit
+fi
 
 echo_info "Connecting to $host as $user"
 if [ "$#" -eq 0 ]; then
