c7ac1db7f1b1ff7ef9ef2a1c7e697cac14033a2f
Martin Oehler <mail@martinoehler.de>
2018-09-12 15:27:21 +0200

    Revert "changed broadcast to execute commands in screen"
    This reverts commit aebf4144d27e94d58741597cfba9a65b3bffb3d1.

--- scripts/broadcast.sh
+++ scripts/broadcast.sh
@@ -6,23 +6,16 @@ if [ "$#" -eq 0 ]; then
     echo "Usage: broadcast <Command>"
     exit 1
 fi
+echo "1"
 command=$@;
-
-if [ -z "$command" ]; then
-  echo_error "No command specified"
-  exit
-fi
 
 hosts=($ROBOT_HOSTNAMES)
 users=($ROBOT_USERS)
-screen_session=$command
-
 
 for idx in "${!hosts[@]}"; do 
     host="${hosts[$idx]}"
     user="${users[$idx]}"
     echo "Executing $command on $host"
-    ssh $user@$host "screen -dmLS $screen_session bash -c $command"
-    
+    $dir/ssh.sh $host $command
 done
 
