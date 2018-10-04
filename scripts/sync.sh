7b4af2f26b4321338afc76d588622509359f40bd
Martin Oehler <mail@martinoehler.de>
2018-10-04 11:46:37 +0200

    fixed typo in environment variable

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,16 +2,16 @@
 
 source $ROSWSS_BASE_SCRIPTS/helper/helper.sh
 
-# only execute if ROBOT_MASTER_HOSTNANE is set
-if [ -z "$ROBOT_MASTER_HOSTNANE" ]; then
-    echo_error "ERROR: In order to use the sync command, please set ROBOT_MASTER_HOSTNANE." 
+# only execute if ROBOT_MASTER_HOSTNAME is set
+if [ -z "$ROBOT_MASTER_HOSTNAME" ]; then
+    echo_error "ERROR: In order to use the sync command, please set ROBOT_MASTER_HOSTNAME" 
     exit 1
 fi
 
 sudo /etc/init.d/chrony stop
 
 if [ "$#" -lt 1 ]; then
-  sudo ntpdate $ROBOT_MASTER_HOSTNANE
+  sudo ntpdate $ROBOT_MASTER_HOSTNAME
 else
   sudo ntpdate $1
 fi
