b70a87d24ee246bf9f1c3593acb6daf6e41414ff
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2021-03-02 10:53:01 +0100

    Fix update_make script

--- scripts/update_make.sh
+++ scripts/update_make.sh
@@ -3,9 +3,12 @@ set -e
 
 for dir in ${ROSWSS_SCRIPTS//:/ }; do
     if [ -r "$dir/update.sh" ]; then
+        echo  "$dir/update.sh"
         $dir/update.sh
     fi
+done
 
+for dir in ${ROSWSS_SCRIPTS//:/ }; do
     if [ -r "$dir/make.sh" ]; then
         $dir/make.sh "$@"
     fi
