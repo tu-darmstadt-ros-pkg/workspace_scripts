ca61d605680c191eaccb9e6f2d22a60fa5d46e4c
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2021-06-18 17:33:56 +0200

    automate rosdep install

--- scripts/update.sh
+++ scripts/update.sh
@@ -151,8 +151,8 @@ else
 
     echo_info ">>> Updating rosdeps for all packages in workspace"
     if [[ $_NO_SUDO == 1 ]]; then
-        rosdep check --ignore-src -r --from-paths .
+        rosdep check --ignore-src -y -r --from-paths .
     else
-        rosdep install --ignore-src -r --from-paths .
+        rosdep install --ignore-src -y -r --from-paths .
     fi
 fi
