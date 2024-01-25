a98cdb5306414bc9136b96191e37dab909e0e95f
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2024-01-25 11:39:34 +0100

    Fixes for 20.04.

--- scripts/cross_compile.py
+++ scripts/cross_compile.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # PYTHON_ARGCOMPLETE_OK
 import argparse
+
 try:
-  import argcomplete
-  __argcomplete = True
+    import argcomplete
+
+    __argcomplete = True
 except ImportError:
-  __argcomplete = False
+    __argcomplete = False
 from catkin_pkg.packages import find_packages
 from datetime import datetime, timezone
 from dateutil.parser import parse as parse_date
@@ -18,13 +20,13 @@ import sys
 
 
 class PkgChoicesCompleter:
-  def __init__(self, workspace_path):
-    self.workspace_path = workspace_path
-    self.directory_completer = argcomplete.completers.DirectoriesCompleter()
+    def __init__(self, workspace_path):
+        self.workspace_path = workspace_path
+        self.directory_completer = argcomplete.completers.DirectoriesCompleter()
 
-  def __call__(self, **_):
-    packages = find_packages(self.workspace_path)
-    return [packages[path].name for path in packages]
+    def __call__(self, **_):
+        packages = find_packages(self.workspace_path)
+        return [packages[path].name for path in packages]
 
 
 def cross_compile(
@@ -32,14 +34,17 @@ def cross_compile(
     packages: Union[str, List[str]],
     ros_distro: str,
     platform: str,
+    output_dir: str,
     build_type: str = "RelWithDebInfo",
     clean: bool = False,
     no_cache: bool = False,
-    rebuild: bool = False,
+    rebuild: bool = False
 ) -> bool:
     # if packages is iterable, join them
     if isinstance(packages, list):
         packages = " ".join(packages)
+    print(">>> Create output directory")
+    os.makedirs(output_dir, exist_ok=True)
     tag = f"cross-compile-{ros_distro}-{platform.replace('/', '_')}"
     docker_client = docker.from_env()
     print(f">>> Obtain image for {ros_distro} on {platform}")
@@ -59,7 +64,7 @@ def cross_compile(
             rebuild = True
     if rebuild:
         path = os.path.join(
-            getenv("TUDA_WSS_BASE", os.path.dirname(os.path.dirname(__file__))),
+            os.path.dirname(getenv("ROSWSS_BASE_SCRIPTS", os.path.dirname(__file__))),
             "docker",
             "cross_compile",
         )
@@ -74,7 +79,6 @@ def cross_compile(
                 "USER_ID": f"{getuid()}",
                 "GROUP_ID": f"{getgid()}",
             },
-            network_mode="host",
             platform=platform,
             pull=True,
             rm=True,
@@ -99,16 +103,20 @@ def cross_compile(
     try:
         tmp_path = f"/tmp/tudawss/{tag}"
         if clean:
+
             def ignore_file_not_found(func, path, exc_info):
                 if isinstance(exc_info[1], FileNotFoundError):
                     return
                 func(path)
+
             rmtree(f"{tmp_path}/build", onerror=ignore_file_not_found)
+            rmtree(f"{tmp_path}/devel", onerror=ignore_file_not_found)
             rmtree(f"{tmp_path}/install", onerror=ignore_file_not_found)
-            rmtree(f"{tmp_path}/log", onerror=ignore_file_not_found)
+            rmtree(f"{tmp_path}/logs", onerror=ignore_file_not_found)
         os.makedirs(f"{tmp_path}/build", exist_ok=True)
+        os.makedirs(f"{tmp_path}/devel", exist_ok=True)
         os.makedirs(f"{tmp_path}/install", exist_ok=True)
-        os.makedirs(f"{tmp_path}/log", exist_ok=True)
+        os.makedirs(f"{tmp_path}/logs", exist_ok=True)
         container = docker_client.containers.run(
             tag,
             command=f"{packages}",
@@ -117,21 +125,24 @@ def cross_compile(
                     "bind": "/workspace/src",
                     "mode": "ro",
                 },
-                f"{tmp_path}/install": {
-                    "bind": "/workspace/install",
-                    "mode": "rw",
-                },
                 f"{tmp_path}/build": {
                     "bind": "/workspace/build",
                     "mode": "rw",
                 },
-                f"{tmp_path}/log": {
-                    "bind": "/workspace/log",
+                f"{tmp_path}/devel": {
+                    "bind": "/workspace/devel",
+                    "mode": "rw",
+                },
+                f"{tmp_path}/install": {
+                    "bind": "/workspace/install",
+                    "mode": "rw",
+                },
+                f"{tmp_path}/logs": {
+                    "bind": "/workspace/logs",
                     "mode": "rw",
                 },
             },
             environment={"BUILD_TYPE": build_type},
-            network_mode="host",
             platform=platform,
             detach=True,
             stdout=True,
@@ -143,11 +154,14 @@ def cross_compile(
                 print(text)
         result = container.wait()
         if result["StatusCode"] != 0:
-            print(f"Cross-compilation failed with exit code {result['StatusCode']}", file=sys.stderr)
+            print(
+                f"Cross-compilation failed with exit code {result['StatusCode']}",
+                file=sys.stderr,
+            )
             return False
         print(f">>> Copying results")
         # Copy install folder to workspace
-        target_path = os.path.join(workspace_path, "..", "cross-compile-install")
+        target_path = output_dir
         if ros_distro != os.getenv("ROS_DISTRO"):
             target_path = os.path.join(target_path, ros_distro)
         target_path = os.path.join(target_path, platform.replace("/", "_"))
@@ -164,7 +178,9 @@ def cross_compile(
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Builds the given packages for the given platform/architecture in a docker image.")
+    parser = argparse.ArgumentParser(
+        description="Builds the given packages for the given platform/architecture in a docker image."
+    )
     parser.add_argument("--build-type", choices=["Debug", "RelWithDebInfo", "Release"])
     parser.add_argument(
         "--no-cache",
@@ -189,14 +205,20 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--ros-distro",
-        choices=["foxy", "galactic", "humble", "iron"],
+        choices=["noetic"],
         default=getenv("ROS_DISTRO"),
         help="ROS distro to use for cross-compilation. Defaults to ROS distro on host.",
+    )
+    workspace_path = os.environ.get("ROS_WORKSPACE")
+
+    parser.add_argument(
+        "--output-dir",
+        default=os.path.join(os.path.dirname(workspace_path), "cross-compile-install"),
+        help="Specifies the directory where the install output is placed. It will be placed in a subdirectory based on the ROS distro and architecture.",
     )
     package_arg = parser.add_argument(
         "PACKAGE", nargs="*", help="Packages to cross-compile"
     )
-    workspace_path = os.environ.get("ROS_WORKSPACE")
     if __argcomplete:
         package_arg.completer = PkgChoicesCompleter(workspace_path)
         argcomplete.autocomplete(parser)
@@ -209,5 +231,6 @@ if __name__ == "__main__":
         clean=args.clean,
         no_cache=args.no_cache,
         rebuild=args.rebuild,
+        output_dir=args.output_dir
     ):
         exit(1)
