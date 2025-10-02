a44761ce877136dffefb4cf6dd9370200b9ff4e5
Alexander Stumpf <alexander.stumpf@energy-robotics.com>
2025-10-02 21:46:42 +0000

    Add support for plotjuggler configs in ui script. General cleanup of UI command script.

--- scripts/completion/ui.sh
+++ scripts/completion/ui.sh
@@ -5,7 +5,7 @@ function roswss_ui() {
 
     # only execute if UI_LAUNCH_PKG is set
     if [ -z "$UI_LAUNCH_PKG" ]; then
-        echo_error "ERROR: In order to use the ui command, please set UI_LAUNCH_PKG." 
+        echo_error "ERROR: In order to use the ui command, please set UI_LAUNCH_PKG."
         return 1
     fi
 
@@ -17,11 +17,11 @@ function roswss_ui() {
         _roswss_ui_help
         return 0
     fi
-    
+
     if [[ -z "$command" ]]; then
       if [[ -z "$UI_DEFAULT_LAUNCH_FILE" ]]; then
         _roswss_ui_help
-        return 0  
+        return 0
       else
         roslaunch $UI_LAUNCH_PKG $UI_DEFAULT_LAUNCH_FILE "$@"
         return 0
@@ -51,111 +51,164 @@ function roswss_ui() {
             roslaunch $UI_LAUNCH_PKG rviz.launch "$@"
         fi
         return 0
+    elif [[ "$command" == "plotjuggler" ]]; then
+        if [[ -n "$1" ]]; then
+            config="$1"
+            shift
+            roslaunch $UI_LAUNCH_PKG plotjuggler.launch plotjuggler_layout_path:=${path}/config/plotjuggler/${config}.xml "$@"
+        else
+            roslaunch $UI_LAUNCH_PKG plotjuggler.launch "$@"
+        fi
+        return 0
     else
       roslaunch $UI_LAUNCH_PKG ${command}.launch "$@"
       return 0
     fi
 
     echo_error "Unknown command: $command"
-    _roswss_ui_help 
-}
-
-function _roswss_ui_rqt_config_files() {
-    local ROSWSS_ROSINSTALL_FILES
-    ROSWSS_ROSINSTALL_FILES=()
-
-    local path
-    path="$(rospack find $UI_LAUNCH_PKG)/config/rqt/"
- 
-    # find all rosinstall files
-    for i in `find -L $path -type f -name "*.perspective"`; do
-        local file
-        file=${i#$path}
-        file=${file%.perspective}
-        if [ -r $i ]; then
-            ROSWSS_ROSINSTALL_FILES+=($file)
-        fi
-    done
-    
-    echo ${ROSWSS_ROSINSTALL_FILES[@]}
-}
-
-function _roswss_ui_rviz_config_files() {
-    local ROSWSS_ROSINSTALL_FILES
-    ROSWSS_ROSINSTALL_FILES=()
-
-    local path
-    path="$(rospack find $UI_LAUNCH_PKG)/config/rviz/"
-
-    # find all bash scripts
-    for i in `find -L $path -type f -name "*.rviz"`; do
-        local file
-        file=${i#$path}
-        file=${file%.rviz}
-        if [ -r $i ]; then
-            ROSWSS_ROSINSTALL_FILES+=($file)
-        fi
-    done
-    
-    echo ${ROSWSS_ROSINSTALL_FILES[@]}
+    _roswss_ui_help
 }
 
 function _roswss_ui_launch_files() {
-    local ROSWSS_ROSINSTALL_FILES
-    ROSWSS_ROSINSTALL_FILES=()
+    local LAUNCH_FILES
+    LAUNCH_FILES=()
 
     local path
     path="$(rospack find $UI_LAUNCH_PKG)/launch/"
- 
-    # find all rosinstall files
+
+    # find all launch files
     for i in `find -L $path -type f -name "*.launch"`; do
         local file
         file=${i#$path}
         file=${file%.launch}
         if [ -r $i ]; then
-            ROSWSS_ROSINSTALL_FILES+=($file)
+            LAUNCH_FILES+=($file)
         fi
     done
-    
-    echo ${ROSWSS_ROSINSTALL_FILES[@]}
+
+    echo ${LAUNCH_FILES[@]}
+}
+
+function _roswss_ui_rqt_config_files() {
+    local CONFIG_FILES
+    CONFIG_FILES=()
+
+    local path
+    path="$(rospack find $UI_LAUNCH_PKG)/config/rqt/"
+
+    # check if path exists
+    if [ -d "$path" ]; then
+      # find all rqt perspective files
+      for i in `find -L $path -type f -name "*.perspective"`; do
+          local file
+          file=${i#$path}
+          file=${file%.perspective}
+          if [ -r $i ]; then
+              CONFIG_FILES+=($file)
+          fi
+      done
+    fi
+
+    echo ${CONFIG_FILES[@]}
+}
+
+function _roswss_ui_rviz_config_files() {
+    local CONFIG_FILES
+    CONFIG_FILES=()
+
+    local path
+    path="$(rospack find $UI_LAUNCH_PKG)/config/rviz/"
+
+    # check if path exists
+    if [ -d "$path" ]; then
+      # find all rviz files
+      for i in `find -L $path -type f -name "*.rviz"`; do
+          local file
+          file=${i#$path}
+          file=${file%.rviz}
+          if [ -r $i ]; then
+              CONFIG_FILES+=($file)
+          fi
+      done
+    fi
+
+    echo ${CONFIG_FILES[@]}
+}
+
+function _roswss_ui_plotjuggler_layout_files() {
+    local CONFIG_FILES
+    CONFIG_FILES=()
+
+    local path
+    path="$(rospack find $UI_LAUNCH_PKG)/config/plotjuggler/"
+
+    # check if path exists
+    if [ -d "$path" ]; then
+      # find all plotjuggler layout files
+      for i in `find -L $path -type f -name "*.xml"`; do
+          local file
+          file=${i#$path}
+          file=${file%.xml}
+          if [ -r $i ]; then
+              CONFIG_FILES+=($file)
+          fi
+      done
+    fi
+
+    echo ${CONFIG_FILES[@]}
 }
 
 function _roswss_ui_commands() {
     local ROSWSS_COMMANDS
-    ROSWSS_COMMANDS=('help' 'rqt' 'rviz')
-    
+    ROSWSS_COMMANDS=('help' 'rqt' 'rviz' 'plotjuggler')
+
     echo ${ROSWSS_COMMANDS[@]}
 }
 
 function _roswss_ui_help() {
     local commands
 
-    echo_note "The following commands are available:"
     commands=$(_roswss_ui_commands)
+    echo_note "The following commands are available:"
     for i in ${commands[@]}; do
         echo "   $i"
     done
     echo
 
-    echo_note "The following launch files are available:"
     commands=$(_roswss_ui_launch_files)
-    for i in ${commands[@]}; do
-        echo "   $i"
-    done
-    echo
+    if [ -n "$commands" ]; then
+        echo_note "The following launch files are available:"
+        for i in ${commands[@]}; do
+            echo "   $i"
+        done
+        echo
+    fi
 
-    echo_note "The following rqt perspectives are available:"
     commands=$(_roswss_ui_rqt_config_files)
-    for i in ${commands[@]}; do
-        echo "   $i"
-    done
-    echo
+    if [ -n "$commands" ]; then
+        echo_note "The following rqt perspectives are available:"
+        for i in ${commands[@]}; do
+            echo "   $i"
+        done
+        echo
+    fi
 
-    echo_note "The following rviz defaults are available:"
     commands=$(_roswss_ui_rviz_config_files)
-    for i in ${commands[@]}; do
-        echo "   $i"
-    done
+    if [ -n "$commands" ]; then
+        echo_note "The following rviz defaults are available:"
+        for i in ${commands[@]}; do
+            echo "   $i"
+        done
+    fi
+
+    commands=$(_roswss_ui_plotjuggler_layout_files)
+    if [ -n "$commands" ]; then
+        echo_note "The following plotjuggler layouts are available:"
+        for i in ${commands[@]}; do
+            echo "   $i"
+        done
+        echo
+    fi
 }
 
 function _roswss_ui_complete() {
@@ -164,7 +217,7 @@ function _roswss_ui_complete() {
     # only execute if UI_LAUNCH_PKG is set
     if [ -z "$UI_LAUNCH_PKG" ]; then
         echo
-        echo_error "ERROR: In order to use the ui command, please set UI_LAUNCH_PKG." 
+        echo_error "ERROR: In order to use the ui command, please set UI_LAUNCH_PKG."
         return 1
     fi
 
@@ -201,8 +254,12 @@ function _roswss_ui_complete() {
                 COMPREPLY=( $( compgen -W "$(_roswss_ui_rviz_config_files)" -- "$cur" ) )
                 ;;
 
+            plotjuggler)
+                COMPREPLY=( $( compgen -W "$(_roswss_ui_plotjuggler_layout_files)" -- "$cur" ) )
+                ;;
+
             *)
-                COMPREPLY=()             
+                COMPREPLY=()
                 ;;
         esac
     fi
